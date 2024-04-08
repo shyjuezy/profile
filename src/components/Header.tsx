30b93e44a84f2feef23e30fca689c0ed3bbb3d10
Shyju Viswambaran <shyjuezy@gmail.com>
2024-04-07 23:25:56 -0500

    Updated the design

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <header className="flex mt-8 bg-white sm:pl-4 md:pl-0">
+    <header className="flex mt-0 bg-white sm:px-4 lg:mt-8 md:px-0">
       <div className="hidden md:block flex-none w-16 bg-green-500 p-4 mr-2 rounded-r-lg"></div>
-      <div className="flex-grow mx-auto overflow-hidden bg-white shadow-lg rounded-l-lg">
-        <div className="flex items-center min-h-36 pr-8 md:pr-32">
+      <div className="flex-grow mx-auto overflow-hidden bg-white shadow-lg rounded-lg border-b border-grey-500">
+        <div className="flex items-center min-h-36 pr-8 lg:pr-24 ">
           <div
-            className="hidden sm:block h-36 w-48 relative bg-amber-800 rounded-lg shadow-lg overflow-hidden"
+            className="hidden sm:block h-36 w-48 relative bg-white rounded-lg shadow-lg overflow-hidden"
             style={{ clipPath: "polygon(0 0, 100% 0, 85% 100%, 0% 100%)" }}
           >
             <div
@@ -15,7 +15,7 @@ export default function Header() {
               // style={{ transform: "scale(1.5) translateY(-5%)" }}
             >
               <Image
-                src="/portfolio.jpeg"
+                src="/portfolio.png"
                 alt="profile"
                 layout="fill"
                 objectFit="cover"
@@ -24,8 +24,8 @@ export default function Header() {
             </div>
           </div>
           <div className="ml-4 flex-grow self-end pb-2">
-            <div className="text-xl font-semibold text-gray-800">Shyju</div>
-            <div className="text-xl font-semibold text-green-700">
+            <div className="text-2xl font-semibold text-gray-800">Shyju</div>
+            <div className="font-crimson text-2xl font-semiold text-green-700">
               Viswambaran
             </div>
             <div className="text-sm text-gray-700">Sr Fullstack Developer</div>
@@ -42,6 +42,7 @@ export default function Header() {
           </div>
         </div>
       </div>
+      <div className="hidden md:block flex-none w-16 bg-green-500 ml-2 rounded-l-lg"></div>
     </header>
   );
 }
